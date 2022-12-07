d5a383f9e02148cf549ee0996d263de4d978e700
nguba2000 <114119499+nguba2000@users.noreply.github.com>
2022-12-06 20:59:21 -0800

    delete

--- mysite/second_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-#localhost:8000/second_app/
-urlpatterns = [
-path('',view=views.index, name='index'), #localhost 8000/second_app/
-]
--- mysite/second_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-
-# Create your views here.
-def index(request):
-    return render(request=request, template_name='second_app/index.html') 
